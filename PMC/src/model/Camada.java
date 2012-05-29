ed681ddc52ed370c9d819d0ca220f2787248d055
douglaseusilva <douglaseusilva@gmail.com>
2012-05-29 16:14:47 +0000

    Prática 3

---
+++ PMC/src/model/Camada.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.Random;
+
+public class Camada {
+	
+	private int numeroDeEntradas;
+	private int numeroDeNeuronios;
+	private float[][] pesos;
+	private Neuronio neuronios[];
+	private Random aleatorio;
+	private float[][] pesosIniciais;
+	
+	
+	public Camada(int entradas, int neuronios){
+		
+		this.numeroDeEntradas = entradas;
+		this.numeroDeNeuronios = neuronios;
+		this.neuronios = new Neuronio[numeroDeNeuronios];
+		this.pesos = new float[numeroDeNeuronios][numeroDeEntradas];
+					
+	}
+	
+	/**
+	 * Coloca pesos aleatorios no vetor de pesos e guarda uma copia 
+	 * do vetor de pesos inicial.
+	 * 
+	 */
+	public void PesosAleatorios(){
+		this.aleatorio = new Random();
+		for(int a=0; a<numeroDeNeuronios; a++){
+			for (int b=0; b<numeroDeEntradas; b++)
+				pesos[a][b] = aleatorio.nextFloat();
+		}
+		
+		pesosIniciais = pesos;
+	}
+
+	public Neuronio[] getNeuronios() {
+		return neuronios;
+	}
+
+	public void setNeuronios(Neuronio[] neuronios) {
+		this.neuronios = neuronios;
+	}
+
+	public int getNumeroDeNeuronios() {
+		return numeroDeNeuronios;
+	}
+
+	public void setNumeroDeNeuronios(int numeroDeNeuronios) {
+		this.numeroDeNeuronios = numeroDeNeuronios;
+	}
+
+}
