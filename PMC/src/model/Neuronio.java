467e543d488483b81ffcd70b1f0a0f6adbdbdeda
douglaseusilva <douglaseusilva@gmail.com>
2012-06-01 20:22:00 +0000

    FEITO:
    * Pequenas modificações nos imports;
    * Leitura do arquivo está genérica;
    
    FALTA: 
    * Na classe Rede: generalizar (possivelmente usando um if) para o ajuste de pesos
    da primeira camada onde as entradas "Y" não são as saidas da camada anterior
    e sim o valor "X" da camada de entrada.

--- PMC/src/model/Neuronio.java
+++ PMC/src/model/Neuronio.java
@@ -1,10 +1,7 @@
 package model;
 
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Random;
-import java.util.Scanner;
+
 
 /**
  *
--- PMC/src/pmc/StartPMC.java
+++ PMC/src/pmc/StartPMC.java
@@ -1,6 +1,8 @@
 package pmc;
 
+import java.io.*;
 import model.Rede;
+
 /**
  * Classe para inicialização da rede
  * @author Douglas e André
@@ -10,11 +12,19 @@ public class StartPMC {
 
 	/**
 	 * @param args
+	 * @throws Exception 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Rede rede = new Rede(3);
-		rede.treinar();
+//		Rede rede = new Rede(3);
+//		rede.treinar();
+		
+		File arquivoLeitura = new File("treina.txt");  
+		LineNumberReader linhaLeitura = new LineNumberReader(new FileReader(arquivoLeitura));  
+		linhaLeitura.skip(arquivoLeitura.length());  
+		int qtdLinha = linhaLeitura.getLineNumber();
+		System.out.println(qtdLinha);
 	}
 
 }
