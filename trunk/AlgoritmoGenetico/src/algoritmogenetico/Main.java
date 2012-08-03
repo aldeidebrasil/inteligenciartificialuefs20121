ce8866e3199a927fde4a52b1c6a04404c362c447
douglaseusilva@gmail.com <douglaseusilva@gmail.com@07554e6a-2381-2864-ac53-a352f7ec7ab8>
2012-08-03 03:41:14 +0000

    Algoritmo genetico implementado e testando os bugs. Por enquanto os resultados gerados estam bem loucos. Um dos possíveis motivos disso é que o metodo da roleta funciona com a soma de todos os fitness, porém neste caso existe a possibilidade de fitnesse negativo, o que torna a soma dos fitness ineficiente. Eu estava pensando em criar uma variavel chamada chance que seria o fitness somado com 1, retirando a possibilidade de trabalhar com numero negativo.

--- trunk/AlgoritmoGenetico/src/algoritmogenetico/Main.java
+++ trunk/AlgoritmoGenetico/src/algoritmogenetico/Main.java
@@ -7,6 +7,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		AlgoritmoGenetico ag = new AlgoritmoGenetico(100, 22, 200, 0.7f, 0.01f);
+		ag.rodar();
+		System.out.println("Melhor resultado obtido foi: " +
+				ag.getMelhorResultado().getFitness());
 
 	}
 
